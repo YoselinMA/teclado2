14d52e4c8ae8c30c1775c755f1e4dc4547430f59
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-06-07 23:01:12 -0600

    Create teclafdo2.java

---
+++ teclafdo2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class teclado2 {
+    public static void main(String[] args) {
+        Scanner cs = new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Ingresa un numero ");
+        a = cs.nextInt();
+
+    }
+    
+}
